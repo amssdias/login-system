481265c12ad2fc6032305e9ab824e74becc49d21
André Dias <andremssdias@hotmail.com>
2021-07-27 17:56:13 +0200

    Testing models

--- loginSystem/loginSys/tests/test_models.py
+++ loginSystem/loginSys/tests/test_models.py
@@ -14,9 +14,18 @@ class TestModels(TestCase):
         self.assertEquals(user.first_name, 'Andre')
 
     def test_add_user(self):
-        user_2 = MyUser.objects.create(username='ElizeuCarlos', email="eli@gmail.com")
+        user_2 = MyUser.objects.create(
+            username='ElizeuCarlos', 
+            email="eli@gmail.com",
+            first_name="elizeu",
+            last_name="carlos"
+            )
+
         user_2.set_password("elizeu12345")
         user_2.save()
         self.assertEquals(MyUser.objects.count(), 2)
+        self.assertIsInstance(user_2, MyUser)
         self.assertEquals(user_2.email, "eli@gmail.com")
-        
+        self.assertEquals(user_2.first_name, "Elizeu")
+        self.assertEquals(user_2.last_name, "Carlos")
+        self.assertEquals(user_2.is_staff, False)
