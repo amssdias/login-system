cfb8eaa67d49e317be7eda0670247a691369ca75
André Dias <andremssdias@hotmail.com>
2021-07-28 15:12:47 +0200

    Test forms and test models

---
+++ loginSystem/loginSys/tests/test_form.py
@@ -0,0 +1,48 @@
+from logging import log
+from loginSys.views import register
+from django.test import TransactionTestCase 
+from loginSys.forms import LoginForm, RegisterForm
+
+class TestForm(TransactionTestCase):
+
+    def test_login_form_valid_data(self):
+        login_form = LoginForm(data={
+            'username': 'andre',
+            'password': 'diasdias'
+        })
+
+        self.assertTrue(login_form.is_valid())
+
+    def test_login_form_invalid_data(self):
+        login_form = LoginForm(data={})
+
+        self.assertFalse(login_form.is_valid())
+        self.assertEquals(len(login_form.errors), 2)
+
+    def test_register_form_valid_data(self):
+        register_form = RegisterForm(data={
+            'first_name': 'andre',
+            'last_name': 'dias',
+            'username': 'AndreDias',
+            'email': 'andre@gmail.com',
+            'password': 'diasdias',
+            'password_confirmation': 'diasdias'
+        })
+
+        self.assertTrue(register_form.is_valid())
+
+    def test_register_form_invalid_data(self):
+        register_form = RegisterForm(data={})
+        self.assertFalse(register_form.is_valid())
+
+        register_form = RegisterForm(data={
+            'first_name': 'andre',
+            'last_name': 'dias',
+            'username': 'AndreDias',
+            'password': 'diasdias',
+            'password_confirmation': 'dias',
+            'email': 'andre@hotmail.com'
+        })
+
+        self.assertFalse(register_form.is_valid())
+        self.assertEquals(len(register_form.errors), 2)
--- loginSystem/loginSys/tests/test_models.py
+++ loginSystem/loginSys/tests/test_models.py
@@ -19,13 +19,34 @@ class TestModels(TestCase):
             email="eli@gmail.com",
             first_name="elizeu",
             last_name="carlos"
-            )
+        )
 
         user_2.set_password("elizeu12345")
         user_2.save()
+
         self.assertEquals(MyUser.objects.count(), 2)
         self.assertIsInstance(user_2, MyUser)
         self.assertEquals(user_2.email, "eli@gmail.com")
         self.assertEquals(user_2.first_name, "Elizeu")
         self.assertEquals(user_2.last_name, "Carlos")
         self.assertEquals(user_2.is_staff, False)
+
+        user_3 = MyUser.objects.create(
+            username='SamLagrosse', 
+            email="samuel@gmail.com",
+            first_name="sam",
+            last_name="lagrosse"
+        )
+        user_3.set_password("sam123456")
+        user_3.save()
+
+        self.assertEquals(MyUser.objects.count(), 3)
+        self.assertIsInstance(user_3, MyUser)
+        self.assertEquals(user_3.email, "samuel@gmail.com")
+        self.assertEquals(user_3.first_name, "Sam")
+        self.assertEquals(user_3.last_name, "Lagrosse")
+        self.assertEquals(user_3.is_staff, False)
+
+    def test_remove_user(self):
+        self.user_1.delete()
+        self.assertEquals(MyUser.objects.count(), 0)
